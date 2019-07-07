436a197eb972cbd2a9875757b9046094f3b652d2
Mukuruku <berteskalukas@gmail.com>
2019-07-08 01:02:01 +0300

    first night

---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BethanysPieShop.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
